a0ab211ad1326d7d999916d0c36ab99f3dc75389
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-12-24 16:13:01 +0100

    Add aliases and update interface commentary

--- bin/represent.sh
+++ bin/represent.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+# Alias of generate.sh
+#
 # Usage:
 # ./bin/represent.sh two-fer ~/folder/to/solution
+# ./bin/represent.sh two-fer ~/folder/to/solution/input ~/folder/to/representer/output
 
 node -r esm -r module-alias/register ./dist/represent.js "$@"
