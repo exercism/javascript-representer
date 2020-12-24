d48fefab659d45865440d20f5c378fdc6eb7b586
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-12-24 19:48:13 +0100

    Fix run.sh by resolving left-behind merge conflict (#9)
    * Resolve merge conflict
    
    * Add CHANGELOG entry
    
    * Bump the version

--- bin/run.sh
+++ bin/run.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env sh
 
-<<<<<<< HEAD
-# Usage:
-# ./bin/generate.sh two-fer ~/folder/to/solution
-# ./bin/generate.sh two-fer ~/folder/to/solution/input ~/folder/to/representer/output
-=======
 # Alias of generate.sh
 #
 # Usage:
 # ./bin/run.sh two-fer ~/folder/to/solution
 # ./bin/run.sh two-fer ~/folder/to/solution/input ~/folder/to/representer/output
->>>>>>> b99c491 (Add aliases and update interface commentary)
 
 node -r esm -r module-alias/register ./dist/represent.js "$@"
